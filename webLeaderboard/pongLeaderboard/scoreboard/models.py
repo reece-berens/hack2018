39caac5072a597e2820e13ad5000d4571c51a612
Reece Berens <rberens@ksu.edu>
2018-10-20 08:25:16 -0500

    Make score model for web server

--- webLeaderboard/pongLeaderboard/scoreboard/models.py
+++ webLeaderboard/pongLeaderboard/scoreboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class userScore(models.Model):
+	initials = models.CharField(max_length=2)
+	score = models.IntegerField(default=0)
+	
+	def __str__(self):
+		return self.initials + '-' + self.score
