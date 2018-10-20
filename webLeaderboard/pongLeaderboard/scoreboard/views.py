72c449916f8f6f391582f668036145414afdc7b4
Reece Berens <rberens@ksu.edu>
2018-10-20 11:37:26 -0500

    Json serialized get data

--- webLeaderboard/pongLeaderboard/scoreboard/views.py
+++ webLeaderboard/pongLeaderboard/scoreboard/views.py
@@ -1,11 +1,25 @@
 from django.http import HttpResponse
 from .models import userScore
+from django.core import serializers
 
 def index(request):
     return HttpResponse("Index of leaderboard")
 
 def get(request):
-	return HttpResponse(userScore.objects.all())
+	"""
+	ordered_scores = (userScore.objects
+		.order_by('-score')
+		.values_list('score', flat=True)
+		.distinct())
+	top_records = (userScore.objects
+		.order_by('-score')
+		.filter(score__in=ordered_scores[:5]))
+	"""
+	top_scores = serializers.serialize('json', userScore.objects.order_by('-score')[:5], fields=('initials', 'score'))
+	#score_list = []
+	#for score in top_scores:
+	#	score_list.append(score)
+	return HttpResponse(top_scores)
 
 def post(request, initialsParam, scoreParam):
 	#put object in database
