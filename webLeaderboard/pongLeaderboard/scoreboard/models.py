1b6583b2cbd353a8c1ce139acdaa515df2377dd9
Reece Berens <rberens@ksu.edu>
2018-10-20 20:21:15 -0500

    Change max value for initials

--- webLeaderboard/pongLeaderboard/scoreboard/models.py
+++ webLeaderboard/pongLeaderboard/scoreboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class userScore(models.Model):
-	initials = models.CharField(max_length=2)
+	initials = models.CharField(max_length=9)
 	score = models.IntegerField(default=0)
 	
 	def __str__(self):
